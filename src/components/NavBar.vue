8cc3e4b7fbe2f04de2df8aa3fc0bec493cc10fc9
Joscelyn James <josce.james7@hotmail.com>
2019-04-29 20:35:01 -0600

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="nav">
+      <router-link to="/">Tanks</router-link> |
+      <router-link to="/Treatments">Treatments</router-link> |
+      <router-link to="/about">About</router-link> 
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.component('nav-bar', {
+  renderer: () => {},
+});
+</script>
+
+<style lang="scss">
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
